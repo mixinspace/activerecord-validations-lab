11add4b81fdd0bbcaef1ac3890b3e44cbef88b93
mixinspace <islammadkaur@gmail.com>
2020-11-29 14:24:54 -0600

    commit tree a9527320eddac5791ce0c8cb777bb962bc0caa7a

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates :name, uniqueness: true
+    validates :name, presence: true
+    validates :name, length: { minimum: 2}
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait?
+
+    
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/,
+        /Secret/,
+        /Top [0-9]/,
+        /Guess/
+      ]
+    
+      def is_clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
